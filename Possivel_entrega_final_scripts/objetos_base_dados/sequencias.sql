6107468bb4bb160de0e433d7823ccf4ec3e1af0a
Raster728 <rafael.figueiredo2001@gmail.com>
2024-01-19 16:09:33 +0000

    seq

--- Possivel_entrega_final_scripts/objetos_base_dados/sequencias.sql
+++ Possivel_entrega_final_scripts/objetos_base_dados/sequencias.sql
@@ -98,7 +98,7 @@ CREATE SEQUENCE IF NOT EXISTS public.fatura_encomenda_id_sequence
     MAXVALUE 9223372036854775807
     CACHE 1;
 	
-CREATE SEQUENCE IF NOT EXISTS public.fatura_venda_id_sequence
+CREATE SEQUENCE IF NOT EXISTS public.itens_fatura_venda_id_sequence
     INCREMENT 1
     START 1
     MINVALUE 1

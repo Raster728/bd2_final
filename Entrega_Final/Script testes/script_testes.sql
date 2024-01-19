d0f30cf1f4dab071c341cda881a367702aa140ff
Raster728 <rafael.figueiredo2001@gmail.com>
2024-01-19 19:36:15 +0000

    Entrega Final

--- Entrega_Final/Script testes/script_testes.sql
+++ Entrega_Final/Script testes/script_testes.sql
@@ -2,47 +2,19 @@
 
 --------------------------------- EDITAR -----------------------------------------
 
-SELECT * from public.componentes_em_stock();
+CALL public.proc_editar_clients(1, 'antonio', '122234');
 
-SELECT * from public.componentes_em_stock_por_comp(1);
-
-SELECT * from public.editar_armazem(1,'XXXXX', 'XY', 'notas x');
-
-SELECT * from public.editar_clients(1,'Manuel', '12345');
-
-SELECT * from public.editar_componentes(1, 'GPU', 'desc x');
-
-SELECT * from public.editar_componetes_producao(1, 1, 1, 10);
-
-SELECT * from public.editar_encomenda(1,1,'novas notas');
-
-SELECT * from public.editar_equipamento(1, 'novo nome', 'novo tipo');
-
-SELECT * from public.editar_fatura_encomenda(1, 1000);
-
-SELECT * from public.editar_fatura_venda(1,1,1,100,2);
-
-SELECT * from public.editar_ficha_producao(1,1,1000);
-
-SELECT * from public.editar_fornecedores(1,'novo nome');
-
-SELECT * from public.editar_item_enc(1,1,1, 3);
-
-SELECT * from public.editar_item_fatura_enc(1,1,1,100);
-
-SELECT * from public.editar_itens_remessa(1,1,1,1,3);
-
-SELECT * from public.editar_mao_obra(1,'novo nome','100','tipo A');
-
-CALL public.proc_editar_clients(1, 'novo nome', '120303020');
-
-CALL public.proc_editar_componentes(1, 'novo nome', 'descrição nova');
+CALL public.proc_editar_componentes(1, 'as', 'es');
 
 CALL public.proc_editar_eq_arm(1);
 
-CALL public.proc_editar_fornecedores(1, 'novo nome');
+CALL public.proc_editar_fornecedores(1, 'Eu');
 
 ------------------------------ EXIBIR -----------------------------------
+
+SELECT * from public.componentes_em_stock();
+
+SELECT * from public.componentes_em_stock_por_comp(1);
 
 SELECT * from public.exibir_armazem();
 
@@ -68,8 +40,6 @@ SELECT * from public.exibir_guia_remessa();
 
 SELECT * from public.exibir_itens_encomenda();
 
-SELECT * from public.exibir_itens_encomenda_com_id_item();
-
 SELECT * from public.exibir_itens_remessa();
 
 SELECT * from public.exibir_mao_obra_por_fp();
@@ -82,15 +52,11 @@ SELECT * from public.func_armazem_id(1);
 
 SELECT * from public.func_cliente_id(1);
 
-SELECT * from public.func_cliente_nome('1234');
+SELECT * from public.func_cliente_nif('1234');
 
 SELECT * from public.func_comp_ficha_id_com_id(1);
 
 SELECT * from public.func_componente_nome(1);
-
-SELECT * from public.func_encomenda_id(1);
-
-SELECT * from public.func_equipamento_armazenado_id(1);
 
 SELECT * from public.func_equipamento_nome(1);
 
@@ -101,8 +67,6 @@ SELECT * from public.func_fornecedores_id(1);
 SELECT * from public.func_itens_enc_id(1);
 
 SELECT * from public.func_itens_enc_id_com_id(1);
-
-SELECT * from public.func_itens_remessa_id(1);
 
 SELECT * from public.func_itens_remessa_id_com_id(1);
 
